eaee2577c7c1045f14c46c73e34846bb3a8bc4c5
BogaSupriya <132595486+BogaSupriya@users.noreply.github.com>
2024-12-19 20:52:51 -0600

    created a webpage and making dynamic content

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def home(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    return render(request, 'home.html',{'age':'23'})
 
 
 
